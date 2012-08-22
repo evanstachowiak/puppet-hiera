58928ab7ae777c06a04585f8111e6a0224866a2e
Evan Stachowiak <evan.stachowiak@gmail.com>
2012-08-22 11:46:27 -0400

    added initial files

---
+++ manifests/init.pp
@@ -0,0 +1,35 @@
+# Class hiera 
+#
+# Installs hiera
+#
+# == Parameters
+#
+#   [*ensure*]
+#     Sets whether hiera is installed.
+#     Default value: 'present'
+#   [*package_name*]
+#     Name of the hiera package
+#     Default value: 'hiera'
+#   [*provider*]
+#     Provider to install hiera
+#     Default value: 'gem'
+#
+# == Examples
+#
+#   class { 'hiera': }
+#
+# == Requires:
+#
+#
+class hiera (
+  $ensure       = 'present',
+  $package_name = $hiera::params::hiera_package_name,
+  $provider     = $hiera::params::provider
+) inherits params {
+
+  package { $package_name:
+    ensure   => $ensure,
+    provider => $provider,
+  } 
+
+}
